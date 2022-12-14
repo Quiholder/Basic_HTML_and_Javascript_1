fd80d48d78c3f50cf120e818fecaa99f51ad7e9a
Quiholder <119394603+Quiholder@users.noreply.github.com>
2022-12-13 21:12:38 -0500

    Add files via upload
    add javascript file

---
+++ basic_javascript_1.js
@@ -0,0 +1,8 @@
+alert ("Hello, Quiana! I am an alert!"); 
+<script>
+   function My_first_function(){
+     var string= "kiss me, I'm Irish!";
+     var result= string.fontcolor("green");
+     document.getElementId("Irish").innerHTRML=result;
+}
+</script>
